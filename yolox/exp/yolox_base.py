8e36fbab7ab2eeb3125ca3ae3df63c1d1d822915
Seokhee Han <hsh060824@gmail.com>
2023-08-05 14:28:55 +0900

    230804_PM2
    max epoch at 100

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -71,7 +71,7 @@ class Exp(BaseExp):
         # epoch number used for warmup
         self.warmup_epochs = 5
         # max training epoch
-        self.max_epoch = 1000
+        self.max_epoch = 100
         # minimum learning rate during warmup
         self.warmup_lr = 0
         self.min_lr_ratio = 0.05
