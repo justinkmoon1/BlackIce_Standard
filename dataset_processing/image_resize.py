e18efb1dcac9a5b7d228278d0406f06e65eebf46
justinkmoon1 <87982180+justinkmoon1@users.noreply.github.com>
2023-08-16 22:46:26 +0900

    data resize

--- dataset_processing/image_resize.py
+++ dataset_processing/image_resize.py
@@ -1,11 +1,11 @@
 from PIL import Image
 
 # 원본 이미지 열기
-image = Image.open("assets/blackice_backgrounddiff.jpg")
+image = Image.open("datasets/COCO/train2017/IMG_0990_JPG.rf.61cba2f4f2ad1faee43de902dca15aed.jpg")
 
 # 새로운 크기로 이미지 조정
-new_size = (640, 640)  # 새로운 크기 (가로, 세로)
+new_size = (416, 416)  # 새로운 크기 (가로, 세로)
 resized_image = image.resize(new_size)
 
 # 조정된 이미지 저장
-resized_image.save("assets/blackice_backgrounddiff.jpg") 
+resized_image.save("datasets/COCO/train2017/IMG_0990_JPG.rf.61cba2f4f2ad1faee43de902dca15aed.jpg") 
