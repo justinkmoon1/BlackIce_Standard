737c9d109041d1c644825b9ae86bc87c38fe91fd
justinkmoon1 <87982180+justinkmoon1@users.noreply.github.com>
2023-08-23 13:34:40 +0900

    fixed for colab bicycle data training

--- yolox/data/dataloading.py
+++ yolox/data/dataloading.py
@@ -21,7 +21,7 @@ def get_yolox_datadir():
     this function will return value of the environment variable. Otherwise, use data
     """
     yolox_datadir = os.getenv("YOLOX_DATADIR", None)
-    #yolox_datadir = "/content/drive/MyDrive/AIHUB_Data"
+    yolox_datadir = "/content/drive/MyDrive/AIHUB_Data/Experiment"
 
     if yolox_datadir is None:
         import yolox
